60dd35aeba4f15172fa789abbfccd61657b06765
Samuel Lajara <lajara.agullo@gmail.com>
2024-04-18 21:58:19 +0200

    hardhat-ignition deploy

--- MrCrypto-Smart-Contracts/hardhat.config.ts
+++ MrCrypto-Smart-Contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition-ethers";
 import dotenv from "dotenv";
 import "solidity-coverage";
 import "hardhat-gas-reporter";
@@ -8,7 +9,7 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    polygonTesnet: {
+    polygonTestnet: {
       url: process.env.PROVIDER_URL_AMOY!,
       accounts: [process.env.PRIVATE_KEY!],
     },
